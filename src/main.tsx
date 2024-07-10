3882494171eee049ce6efb2e366df7c61e44ea2b
BiarqGabriel <gabriel.martinezp@externos.bci.cl>
2024-07-10 17:46:22 -0400

    chore: Update npm dependencies
    This commit updates the npm dependencies in the project. Specifically, it adds the "react-router-dom" package with version 6.24.1. This package is required for implementing routing functionality in the application. Additionally, it updates the "react-router" package to version 6.24.1 and adds the "@remix-run/router" package with version 1.17.1 as a dependency for "react-router-dom". These updates ensure compatibility and stability with the latest versions of React and other related packages.
    
    Note: This commit message follows the established convention of starting with a verb in the imperative mood, followed by a brief description of the changes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Main } from './pages/Main'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router/router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Main />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
